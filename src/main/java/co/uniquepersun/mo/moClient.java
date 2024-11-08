4b00cff121a24a89bcf45b0393095e04d84cd16e
abhay <abhay.i0am@gmail.com>
2024-11-08 23:43:43 +0530

    basic changements..

---
+++ src/main/java/co/uniquepersun/mo/moClient.java
@@ -0,0 +1,11 @@
+package co.uniquepersun.mo;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class moClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
